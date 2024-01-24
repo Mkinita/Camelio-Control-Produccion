f9b5c316de8f9466d86e242ab394ba5633d3a96f
unknown <jerez4959@gmail.com>
2024-01-24 14:19:41 -0300

    quinto

--- pages/api/producto.js
+++ pages/api/producto.js
@@ -1,27 +1,69 @@
+// import { PrismaClient } from "@prisma/client";
+
+// export default async function handler(req, res) {
+//     const prisma = new PrismaClient();
+//     //Obtener Ordenes
+//     const productos = await prisma.producto.findMany({
+//       orderBy: {
+//         id: 'desc',
+//       },
+//     })
+  
+//     res.status(200).json(productos);
+    
+//     //Crear saldoes
+//     if (req.method === "POST") {
+//       const producto = await prisma.producto.create({
+//         data: {
+//             espesor: req.body.espesor,
+//             ancho: req.body.ancho,
+//             largo: req.body.largo,
+//             piezas: req.body.piezas,
+//             detalle: req.body.detalle,
+//         },
+//       });
+//       res.json(producto);
+//     }
+//   }
+
+
 import { PrismaClient } from "@prisma/client";
 
 export default async function handler(req, res) {
-    const prisma = new PrismaClient();
-    //Obtener Ordenes
+  // Crear una nueva instancia de PrismaClient
+  const prisma = new PrismaClient();
+
+  try {
+    // Obtener Productos ordenados por id de forma descendente
     const productos = await prisma.producto.findMany({
       orderBy: {
         id: 'desc',
       },
-    })
-  
+    });
+
+    // Enviar la respuesta con los productos
     res.status(200).json(productos);
-    
-    //Crear saldoes
+
+    // Crear productos si el método de la solicitud es POST
     if (req.method === "POST") {
       const producto = await prisma.producto.create({
         data: {
-            espesor: req.body.espesor,
-            ancho: req.body.ancho,
-            largo: req.body.largo,
-            piezas: req.body.piezas,
-            detalle: req.body.detalle,
+          espesor: req.body.espesor,
+          ancho: req.body.ancho,
+          largo: req.body.largo,
+          piezas: req.body.piezas,
+          detalle: req.body.detalle,
         },
       });
+      // Enviar la respuesta con el producto creado
       res.json(producto);
     }
+  } catch (error) {
+    // Manejar errores de Prisma
+    console.error("Error al realizar la consulta:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  } finally {
+    // Cerrar la conexión de PrismaClient al finalizar la operación
+    await prisma.$disconnect();
   }
+}
