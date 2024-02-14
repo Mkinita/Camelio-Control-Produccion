1dbac40d71ee95c4e48d17a1d5ae759ce66d6efe
unknown <jerez4959@gmail.com>
2024-02-14 01:45:02 -0300

    noveno0

--- pages/cerrar-turno.js
+++ pages/cerrar-turno.js
@@ -9,8 +9,8 @@ import { useEffect, useCallback, useState } from "react"
 
 export default function AdminProducciones() {
 
-  const fetcher = () => axios('/api/produccionactual').then(datos => datos.data)
-  const { data, error, isLoading } = useSWR('/api/produccionactual',fetcher,{refreshInterval: 100} )
+  const fetcher = () => axios('/api/producciones').then(datos => datos.data)
+  const { data, error, isLoading } = useSWR('/api/producciones',fetcher,{refreshInterval: 100} )
 
   const [ users, setUsers ] = useState([])
   const [ search, setSearch ] = useState("")
@@ -22,7 +22,7 @@ export default function AdminProducciones() {
 
 
   //función para traer los datos de la API
-  const URL = '/api/produccionactual'
+  const URL = '/api/producciones'
 
   const showData = async () => {
     const response = await fetch(URL)
@@ -85,9 +85,9 @@ export default function AdminProducciones() {
 
 
     const comprobarPedido = useCallback(() => {
-        return volumen === "" || volumen.length <3;
+        return fecha2 === "" || fecha2.length <3;
         
-    },[volumen])
+    },[fecha2])
 
 
     useEffect(() => {
@@ -121,20 +121,22 @@ export default function AdminProducciones() {
                 onSubmit={AgregarTurno}
             >
                 <div class="grid grid-cols-1 w-1/4 m-auto items-center justify-center p-2 space-x-2 bg-white gap-2">
-                <div class=" bg-gray-100 p-4 w-54 space-x-4 rounded-lg hidden ">
-                        <input class="bg-gray-100 outline-none" type="date" placeholder="Agrega Volumen" value={fecha2} onChange={e => setFecha2(e.target.value)} />
+                <div class=" bg-gray-100 p-4 w-54 space-x-4 rounded-lg hidden">
+                        <input class="bg-gray-100 outline-none " type="date" value={fecha2} onChange={e => setFecha2(e.target.value)} />
                     </div>
-                    <div class=" bg-gray-100 p-4 w-54 space-x-4 rounded-lg hidden ">
+                    <div class=" bg-gray-100 p-4 w-54 space-x-4 rounded-lg hidden">
                         <input class="bg-gray-100 outline-none text-center" type="text" placeholder="Agrega Volumen" value={volumen} onChange={e => setVolumen(e.target.value)} />
                     </div>
-                    <div class="bg-red-600 text-center py-3 px-5 text-white font-semibold rounded-lg hover:shadow-lg transition duration-3000 cursor-pointer">
-                        <input
-                            type="submit"
-                            className= {`${comprobarPedido()}`}
-                            value="Agregar"
-                            disabled={comprobarPedido()}                   
-                        />
-                    </div>
+                    <div className="bg-red-600 text-center py-3 px-5 text-white font-semibold rounded-lg hover:shadow-lg transition duration-3000 cursor-pointer">
+            {fecha2 !== "" && fecha2.length >= 3 && (
+                <input
+                    type="submit"
+                    className={comprobarPedido()}
+                    value="Agregar"
+                    disabled={comprobarPedido()}
+                />
+            )}
+        </div>
                 </div>
             </form> 
         </div>
